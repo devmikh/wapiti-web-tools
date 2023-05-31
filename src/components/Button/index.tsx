3579ceac3daad4327653fdf0a23a775c1c9aba65
devmikh <mishacyb@gmail.com>
2023-05-31 12:42:52 -0700

    Add type specs to Button props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,11 @@
 import styles from './index.module.css'
 
-export default function Button(props: any) {
+export default function Button(props: { label: string, clickHandler: Function}) {
 
     const { label, clickHandler } = props
 
     return (
-        <button onClick={clickHandler} className={styles['button']}>{label}</button>
+        <button onClick={() => clickHandler()} className={styles['button']}>{label}</button>
     )
 }
 
