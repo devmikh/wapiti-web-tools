235566dacaa83dc2c2263a49ebe9a4259a13a289
devmikh <mishacyb@gmail.com>
2023-06-01 15:38:09 -0700

    Add logo and favicon

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,8 +1,10 @@
 import styles from './index.module.css'
+import logo from '../../../public/logo.svg'
 
 const Navbar = () => {
     return (
         <nav className={styles['navbar']}>
+            <img src={logo} alt="logo" height={50} className={styles['logo']}/>
             <h1>Wapiti Web Tools</h1>
         </nav>
     )
