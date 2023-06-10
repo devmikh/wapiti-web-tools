8633cf66c49e79c3a1bace1e734b87aca54ef3aa
devmikh <mishacyb@gmail.com>
2023-06-09 17:14:01 -0700

    Implement ImageCropper tool

--- src/components/tools/ImageCropper/index.tsx
+++ src/components/tools/ImageCropper/index.tsx
@@ -1,82 +1,120 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
+import Cropper, { Area } from 'react-easy-crop'
+import html2canvas from 'html2canvas';
 
-import ReactCrop, { Crop } from 'react-image-crop'
-import 'react-image-crop/dist/ReactCrop.css'
+import uploadIcon from '../../../assets/icons/upload.svg'
+
 import styles from './index.module.css'
 
-export default function ImageCropper() {
+const Output = ({ croppedArea, src, aspect }: any) => {
+    const outputRef = useRef<HTMLDivElement>(null)
 
-    const [src, setSrc] = useState<string | ArrayBuffer | null>(null)
-    const [crop, setCrop] = useState<Crop>()
-    const [image, setImage] = useState<any>(null)
-    const [output, setOutput] = useState<string | null>(null)
-    
-    const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if (e.target.files && e.target.files.length > 0) {
-          const reader = new FileReader()
-          reader.addEventListener('load', () => {
-            setSrc(reader.result)
-          })
-          reader.readAsDataURL(e.target.files[0])
+    const scale = 100 / croppedArea.width
+    const transform = {
+        x: `${-croppedArea.x * scale}%`,
+        y: `${-croppedArea.y * scale}%`,
+        scale,
+        width: "calc(100% + 0.5px)",
+        height: "auto"
+    }
+
+    const imageStyle = {
+        transform: `translate3d(${transform.x}, ${transform.y}, 0) scale3d(${transform.scale},${transform.scale},1)`,
+        width: transform.width,
+        height: transform.height
+    }
+
+    const handleDownload = () => {
+        if (outputRef.current) {
+            html2canvas(outputRef.current).then((canvas: any) => {
+                const url = canvas.toDataURL('image/png')
+                const link = document.createElement('a')
+                link.href = url
+                link.download = 'cropped_image.png'
+                link.click()
+            })
         }
     }
 
-    const cropImage = () => {
-        if (crop) {
-            const canvas = document.createElement('canvas')
-            const scaleX = image.naturalWidth / image.width
-            const scaleY = image.naturalHeight / image.height
-            canvas.width = crop.width
-            canvas.height = crop.height
-            const ctx = canvas.getContext('2d')
+    return (<>
+                <div
+                    className={styles['output']}
+                    style={{ paddingBottom: `${100 / aspect}%` }}
+                    ref={outputRef}
+                >
+                    <img src={src} alt="" style={imageStyle} />
+                </div>
+                <button onClick={handleDownload}>Download</button>
+            </>
+    )
+}
+
+export default function ImageCropper() {
     
-            const pixelRatio = window.devicePixelRatio
-            canvas.width = crop!.width * pixelRatio
-            canvas.height = crop!.height * pixelRatio
-            ctx!.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0)
-            ctx!.imageSmoothingQuality = 'high'
-    
-            ctx!.drawImage(
-                image,
-                crop.x * scaleX,
-                crop.y * scaleY,
-                crop.width * scaleX,
-                crop.height * scaleY,
-                0,
-                0,
-                crop.width,
-                crop.height,
-            )
-    
-            // Converting to base64
-            const base64Image = canvas.toDataURL('image/jpeg')
-            setOutput(base64Image)
+    const [src, setSrc] = useState<string | ArrayBuffer | null>(null)
+    const [crop, setCrop] = useState({ x: 0, y: 0 })
+    const [zoom, setZoom] = useState(1)
+    const [aspect, setAspect] = useState(1 / 1)
+    const [croppedArea, setCroppedArea] = useState<Area | null>(null)
+
+    const aspectRatioOptions = [
+        { label: '1:1', value: 1 / 1 },
+        { label: '16:9', value: 16 / 9 },
+        { label: '4:3', value: 4 / 3 },
+        { label: '3:4', value: 3 / 4 },
+        { label: '9:16', value: 9 / 16 },
+    ]
+
+    const handleFileUpload = (files: FileList | null) => {
+        if (files && files.length > 0) {
+            const reader = new FileReader()
+            reader.addEventListener('load', () => {
+                setSrc(reader.result)
+            })
+            reader.readAsDataURL(files[0])
         }
-        
-    };
+    }
+
+    const handleDragOver = (e: React.DragEvent<HTMLInputElement>) => {
+        e.preventDefault()
+    }
+    
+    const handleDrop = (e: React.DragEvent<HTMLInputElement>) => {
+        e.preventDefault()
+        handleFileUpload(e.dataTransfer.files)
+    }
 
     return (
-        <div className={styles['container']}>
+        <div className={styles['container']} onDrop={handleDrop} onDragOver={handleDragOver}>
             <h1 className='tool-title'>Image Cropper</h1>
-            <div className={styles['file-input-container']}>
-                <input type="file" accept="image/*" onChange={handleFileUpload}/>
-            </div>
+            <label htmlFor='cropper-input-file' className={styles['cropper-input']}>
+                <img src={uploadIcon} height='30' />
+                <span>Upload image to start cropping</span>
+                <input type='file' accept="image/*" id='cropper-input-file' onChange={(e) => handleFileUpload(e.target.files)} className={styles['cropper-input-file']} />
+            </label>
             
-            <div className={styles['cropper-container']}>
-                <div className={styles['input-container']}>
-                    {src ? (
-                    <ReactCrop
-                        crop={crop}
-                        onChange={c => setCrop(c)}
-                        onComplete={cropImage}
-                        className={styles['cropper']}>
-                            <img src={src as string} onLoad={e => setImage(e.target)}  className={styles['input-image']} />
-                    </ReactCrop>) : 'Please upload an image'}
-                </div>
-                <div className={styles['output-container']}>
-                    {output && <img src={output} />}
-                </div>
-                {output && <a href={output} download='cropped-image.png'>Download</a>}
+            <div className={styles['cropper']}>
+                {src ? <Cropper
+                image={src as string}
+                aspect={aspect}
+                crop={crop}
+                zoom={zoom}
+                objectFit='contain'
+                onCropChange={setCrop}
+                onZoomChange={setZoom}
+                onCropAreaChange={(croppedArea) => {
+                    setCroppedArea(croppedArea);
+                }} /> : <div>Your image will be here</div>}
+            </div>
+            <select value={aspect} onChange={(e: any) => setAspect(e.target.value)}>
+                {aspectRatioOptions.map((option) => (
+                    <option key={option.value} value={option.value}>
+                    {option.label}
+                    </option>
+                ))}
+            </select>
+            <div className={styles['viewer']}>
+                <div>{croppedArea && <Output croppedArea={croppedArea} src={src as string} aspect={aspect} />}</div>
             </div>
         </div>
     )
