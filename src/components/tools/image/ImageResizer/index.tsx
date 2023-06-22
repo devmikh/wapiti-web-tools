8eb17830e3b3c5c3da97d4a6f8c2c0ff71533b6e
devmikh <mishacyb@gmail.com>
2023-06-22 10:21:05 -0700

    Make FileUploader a separate component

--- src/components/tools/image/ImageResizer/index.tsx
+++ src/components/tools/image/ImageResizer/index.tsx
@@ -1,13 +1,47 @@
+import { useState } from 'react'
 import usePageTitle from "../../../../hooks/usePageTitle"
+import FileUploader from "../../../core/FileUploader"
 import styles from './index.module.css'
 
 export default function ImageResizer() {
 
+    const [src, setSrc] = useState<string | ArrayBuffer | null>(null)
+    const [fileName, setFileName] = useState<string>('')
+
+    const handleFileUpload = (files: FileList | null) => {
+        if (files && files.length > 0) {
+            const reader = new FileReader()
+            reader.addEventListener('load', () => {
+                
+                setSrc(reader.result)
+            })
+            reader.readAsDataURL(files[0])
+            setFileName(files[0].name)
+        }
+    }
+
+    const handleDragOver = (e: React.DragEvent<HTMLInputElement>) => {
+        e.preventDefault()
+    }
+    
+    const handleDrop = (e: React.DragEvent<HTMLInputElement>) => {
+        e.preventDefault()
+        handleFileUpload(e.dataTransfer.files)
+    }
+
     usePageTitle('Image Resizer | Wapiti Web Tools')
 
     return (
-        <div className={`tool-container ${styles['container']}`}>
+        <div className={`tool-container ${styles['container']}`} onDrop={handleDrop} onDragOver={handleDragOver}>
             <h1 className='tool-title'>Image Resizer</h1>
+            <FileUploader
+                type='image'
+                src={src as string}
+                firstPrompt='Upload image'
+                secondPrompt='Upload another image'
+                handleFileUpload={handleFileUpload}
+            />
+            <div>{fileName}</div>
         </div>
     )
 }
