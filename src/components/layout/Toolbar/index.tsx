be3f177b1169ff881b7caac94fa9d8454525cba6
devmikh <mishacyb@gmail.com>
2023-07-02 11:59:24 -0700

    Add Searchbar component

--- src/components/layout/Toolbar/index.tsx
+++ src/components/layout/Toolbar/index.tsx
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux'
+import Searchbar from '../Searchbar'
 import ToolCategory from '../ToolCategory'
 import ToolLink from '../ToolLink'
 import { State, Category, Tool } from '../../../store/types'
@@ -21,6 +22,7 @@ export default function Toolbar() {
 
     return (
         <div className={styles['toolbar-container']}>
+            <Searchbar />
             {toolCategories}
         </div>
     )
